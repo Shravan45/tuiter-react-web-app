9c66cf08088484cae1b5d4dd5eb9fcfb492d3f48
shravan45 <shravanramdurg45@gmail.com>
2023-02-19 23:55:32 -0500

    a5 Javascript sp22

---
+++ public/tuiter/WhoToFollowList/who.js
@@ -0,0 +1,26 @@
+export default [{
+        avatarIcon: '../../images/javaLogo.png',
+        userName: 'Java',
+        handleName: '@Java'
+    },
+    {
+        avatarIcon: '../../images/relativity.png',
+        userName: 'Relativity Space',
+        handleName: '@relativityspace'
+    },
+    {
+        avatarIcon: '../../images/virginLogo.png',
+        userName: 'Virgin Galactic',
+        handleName: '@virgingalactic'
+    },
+    {
+        avatarIcon: '../../images/nasaLogoImg.png',
+        userName: 'NASA',
+        handleName: '@NASA'
+    },
+    {
+        avatarIcon: '../../images/tesla.png',
+        userName: 'Tesla',
+        handleName: '@Tesla'
+    },
+];
