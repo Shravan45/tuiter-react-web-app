42084a360b1d87ae1cb7e67d8f868920b0e8d654
shravan45 <shravanramdurg45@gmail.com>
2023-03-05 20:33:09 -0500

    a6 React.js sp23

---
+++ src/labs/a6/index.js
@@ -0,0 +1,22 @@
+import {Link}
+  from "react-router-dom";
+import Nav from "../../nav";
+  import Classes from "./classes";
+  import Styles from "./styles";
+  import ConditionalOutput
+  from "./conditional-output";
+
+
+
+function Assignment6() {
+ return(
+  <div>
+   <Nav/>
+   <h1>Assignment 6</h1>
+   <ConditionalOutput/>
+   <Styles/>
+   <Classes/>
+  </div>
+ )
+}
+export default Assignment6;
